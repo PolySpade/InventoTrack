4bcda946840386ac114b7333ccba22a987a919ec
PolySpade <xudonald889@gmail.com>
2024-06-28 13:48:08 +0800

    Sales Chart and New Package

--- frontend/src/components/charts/WeeklySalesChart.jsx
+++ frontend/src/components/charts/WeeklySalesChart.jsx
@@ -1,15 +1,13 @@
-import React, {useEffect,useState} from 'react'
-import { orders } from '../../constants'
-import { formatTimestampDay } from '../../utils'
+import React from 'react';
+import { orders } from '../../constants';
+import { formatTimestampDay } from '../../utils';
+import Chart from "react-apexcharts";
 
 const WeeklySalesChart = () => {
-
-    // const [chartData, setChartData] = useState({})
-    
     const groupByDateAndSum = (orders) => {
-        return orders.reduce( (acc,order) => {
-            const date = formatTimestampDay(order.timestamp)
-            if(!acc[date]) {
+        return orders.reduce((acc, order) => {
+            const date = formatTimestampDay(order.timestamp);
+            if (!acc[date]) {
                 acc[date] = 0;
             }
             acc[date] += order.totalPaid;
@@ -17,20 +15,59 @@ const WeeklySalesChart = () => {
         }, {});
     }
 
-    const groupedData = groupByDateAndSum(orders)
+    const groupedData = groupByDateAndSum(orders);
 
     const chartData = Object.keys(groupedData).map(date => ({
         date: date,
         totalPaid: groupedData[date],
-      }));
-    
+    }));
 
-      
+    const dates = chartData.map(data => data.date);
+    const totalPaid = chartData.map(data => data.totalPaid);
 
-  return (
-    <div>WeeklySalesChart</div>
-    
-  )
+    const options = {
+        chart: {
+            type: 'line',
+            height: 350
+        },
+        xaxis: {
+            categories: dates
+        },
+        stroke: {
+            width: 5
+        },
+        title: {
+            text: "Weekly Sales Data",
+            align: 'left',
+            style: {
+                fontSize: "20px",
+                color: "white",
+            }
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'dark',
+                gradientToColors: [ '#FDD835'],
+                shadeIntensity: 1,
+                type: 'horizontal',
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 100, 100, 100]
+              }
+        }
+    };
+
+    const series = [{
+        name: 'Sales',
+        data: totalPaid
+    }];
+
+    return (
+        <div className='chart'>
+            <Chart options={options} series={series} type="line" />
+        </div>
+    );
 }
 
-export default WeeklySalesChart
+export default WeeklySalesChart;
