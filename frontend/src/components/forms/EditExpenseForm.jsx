513266de7c152c326a502c34c2c682ac0ef8e3af
PolySpade <xudonald889@gmail.com>
2024-07-04 00:46:40 +0800

    Completed edit expense

--- frontend/src/components/forms/EditExpenseForm.jsx
+++ frontend/src/components/forms/EditExpenseForm.jsx
@@ -1,20 +1,69 @@
+import { formatTimestampDay } from '../../utils';
+import { ExpenseContext } from "../../pages/Expenses";
+import { useContext, useState } from 'react';
+import axios from 'axios';
 
-import { expense_types, currency_types } from '../../constants';
+const EditExpenseForm = ({ onClose, _id, timestamp, amount, currency, expensestype, description }) => {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const { currencies: currency_types, expenseTypes: expense_types } = useContext(ExpenseContext);
 
-const EditExpenseForm = ({ onClose, id, date, amount, currency, type, description }) => {
+  const [formValues, setFormValues] = useState({
+    amount,
+    currency,
+    expensestype,
+    description
+  });
+
   const handleCancel = () => {
     onClose();
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Here you would handle the form submission logic, such as updating the expense
+
+    const updatedExpense = {
+      timestamp,
+      amount: formValues.amount,
+      currency: formValues.currency,
+      expensestype: formValues.expensestype,
+      description: formValues.description
+    };
+
+    try {
+      const response = await axios.put(`${API_URL}/expenses/EditExpense/${_id}`, updatedExpense);
+      if (response.status !== 200) {
+        throw new Error('Failed to update expense');
+      }
+      console.log(response.data.message);
+      window.location.reload();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(`${API_URL}/expenses/DeleteExpense/${_id}`);
+      if (response.status !== 200) {
+        throw new Error('Failed to delete expense');
+      }
+      console.log(response.data.message);
+      window.location.reload();
+    } catch (error) {
+      console.error(error.message);
+    }
     onClose();
   };
 
-  const handleDelete = () => {
-    onClose();
-  }
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormValues(prevValues => ({
+      ...prevValues,
+      [name]: value
+    }));
+  };
+
+  const formattedTimestamp = formatTimestampDay(timestamp);
 
   return (
     <div className='fixed inset-0 flex items-center justify-center z-50'>
@@ -24,34 +73,34 @@ const EditExpenseForm = ({ onClose, id, date, amount, currency, type, descriptio
           <h1 className='text-lg pb-1 justify-center flex'>Edit Expense</h1>
           <div className='my-2 flex flex-col'>
             <label className='text-xs' htmlFor='date'>Date</label>
-            <input id="date" type="text" value={date} disabled className="input input-bordered w-full max-w-xs disabled:text-white" />
+            <input id="date" type="text" value={formattedTimestamp} disabled className="input input-bordered w-full max-w-xs disabled:text-white" />
           </div>
           <div className='my-2 flex flex-col'>
             <label className='text-xs' htmlFor='expense_types'>Type</label>
-            <select id="expense_types" defaultValue={type} className="input input-bordered w-full max-w-xs">
+            <select id="expense_types" name="expensestype" value={formValues.expensestype._id} onChange={handleChange} className="input input-bordered w-full max-w-xs">
               <option disabled value="">Select a Type</option>
-              {expense_types.map((item) => <option key={item.id} value={item.id}>{item.name}</option>)}
+              {expense_types.map((item) => <option key={item._id} value={item._id}>{item.name}</option>)}
             </select>
           </div>
           <div className='my-2 flex flex-col'>
             <label className='text-xs' htmlFor='currency'>Currency</label>
-            <select id="currency" defaultValue={currency} className="input input-bordered w-full max-w-xs">
+            <select id="currency" name="currency" value={formValues.currency._id} onChange={handleChange} className="input input-bordered w-full max-w-xs">
               <option disabled value="">Select a Currency</option>
-              {currency_types.map((item) => <option key={item.id} value={item.id}>{item.name}</option>)}
+              {currency_types.map((item) => <option key={item._id} value={item._id}>{item.name}</option>)}
             </select>
           </div>
           <div className='my-2 flex flex-col'>
             <label className='text-xs' htmlFor='amount'>Amount</label>
-            <input id="amount" type="number" defaultValue={amount} className="input input-bordered w-full max-w-xs" />
+            <input id="amount" name="amount" type="number" value={formValues.amount} onChange={handleChange} className="input input-bordered w-full max-w-xs" />
           </div>
           <div className='my-2 flex flex-col'>
             <label className='text-xs' htmlFor='description'>Short Description</label>
-            <input id="description" type="text" defaultValue={description} className="input input-bordered w-full max-w-xs" />
+            <input id="description" name="description" type="text" value={formValues.description} onChange={handleChange} className="input input-bordered w-full max-w-xs" />
           </div>
           <div className="flex flex-row justify-evenly">
             <button type='button' onClick={handleCancel} className="btn text-white my-2">Cancel</button>
             <button type="submit" className="btn text-white my-2">Save</button>
-            <button type="submit" className='btn text-white my-2 bg-error hover:bg-red-400 outline-none border-none'>Delete</button>
+            <button type="button" onClick={handleDelete} className='btn text-white my-2 bg-error hover:bg-red-400 outline-none border-none'>Delete</button>
           </div>
         </form>
       </div>
