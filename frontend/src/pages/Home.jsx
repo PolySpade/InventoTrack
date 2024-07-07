a661fd6ce00c192027444ff0d6d2ec85bde0a947
PolySpade <xudonald889@gmail.com>
2024-07-07 13:02:48 +0800

    Fixed Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 const Home = () => {
   const auth = useAuthUser()
-  console.log(auth.email);
+
   return (
     <>
-      <h1 className=" text-white">Welcome {auth.email}</h1>
+      <h1 className=" text-white">Welcome</h1>
       <Link to="/login">
         <div className="btn btn-secondary">Login</div>
       </Link>
