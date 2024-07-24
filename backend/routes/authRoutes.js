144dce49a5713c388f2801f80fe07b0507d98554
PolySpade <xudonald889@gmail.com>
2024-07-24 19:15:46 +0800

    Removed dot env

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,31 +2,12 @@ import express from 'express';
 import passport from 'passport';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { accountModel } from '../models/accountModel.js';
 import "../strategies/local-strategy.js"; // Ensure this file configures Passport
-import { config } from 'dotenv';
 import authMiddleware from '../middlewares/authMiddleware.js';
-
-config();
-
 
 const router = express.Router();
 const JWT_SECRET = process.env.SECRET;
 
-// const authMiddleware = (req, res, next) => {
-//     const authHeader = req.headers['authorization'];
-//     if (!authHeader) return res.sendStatus(403);
-
-//     const token = authHeader.split(' ')[1];
-//     console.log(token)
-//     if (!token) return res.sendStatus(403);
-
-//     jwt.verify(token, JWT_SECRET, (err, user) => {
-//         if (err) return res.sendStatus(403);
-//         req.user = user;
-//         next();
-//     });
-// };
 
 router.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
@@ -49,7 +30,7 @@ router.get('/status', authMiddleware, (req, res) => {
     return req.user ? res.send(req.user) : res.sendStatus(401);
 });
 
-// If you want to add logout functionality
+
 router.post('/logout', (req, res) => {
     req.logout((err) => {
         if (err) return res.status(400).send({ message: 'Error logging out' });
