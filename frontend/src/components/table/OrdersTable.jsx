5a6cedb1dc3f2238af4cd291e72e6439644841ac
PolySpade <xudonald889@gmail.com>
2024-07-25 17:09:39 +0800

    Fixed Table Pagination

--- frontend/src/components/table/OrdersTable.jsx
+++ frontend/src/components/table/OrdersTable.jsx
@@ -8,15 +8,18 @@ import BulkEditStatusForm from "../forms/BulkEditStatusForm";
 import BulkEditPlatformForm from "../forms/BulkEditPlatformForm";
 
 const ITEMS_PER_PAGE = 10;
+const MAX_PAGE_BUTTONS = 10;
 
 const OrdersTable = () => {
   const { ordersData: orders, refreshData, couriers, salesplatforms } = useContext(OrdersContext);
   const [searchTerm, setSearchTerm] = useState("");
   const [addOrder, setAddOrder] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
+  const [inputPage, setInputPage] = useState('');
   const [orderDetailsId, setOrderDetailsId] = useState(null);
   const [filterStatus, setFilterStatus] = useState("");
   const [filterDate, setFilterDate] = useState("");
+  const [filterPlatform, setFilterPlatform] = useState("");
   const [checkedItems, setCheckedItems] = useState([]);
   const [changeStatus, setChangeStatus] = useState(false);
   const [changePlatform, setChangePlatform] = useState(false);
@@ -27,19 +30,24 @@ const OrdersTable = () => {
     const path = location.pathname.split('/');
     if (path[2] === "Today") {
       setFilterDate(new Date().toISOString().split('T')[0]);
-    }
-    else if(path[2] === "ToProcess"){  
-      setFilterStatus("To Process")
-    }
-    else if (path[2]) {
+    } else if (path[2] === "ToProcess") {
+      setFilterStatus("To Process");
+    } else if (path[2]) {
       setFilterStatus(path[2]);
     }
-    
-    if(path[2] === "search"){
+
+    if (path[2] === "search") {
       setSearchTerm(path[3]);
       setFilterStatus("");
     }
 
+    if (path[2] === "ShippedToday") {
+      setFilterStatus("Shipped Today");
+    }
+
+    if (path[2] === "CancelledToday") {
+      setFilterStatus("Cancelled Today");
+    }
   }, [location]);
 
   const handleSearchChange = (event) => {
@@ -57,6 +65,11 @@ const OrdersTable = () => {
     setCurrentPage(1); // Reset to first page on filter change
   };
 
+  const handlePlatformFilterChange = (platform) => {
+    setFilterPlatform(platform);
+    setCurrentPage(1); // Reset to first page on filter change
+  };
+
   const handleCheckboxChange = (orderId) => {
     setCheckedItems((prev) =>
       prev.includes(orderId) ? prev.filter((item) => item !== orderId) : [...prev, orderId]
@@ -65,19 +78,43 @@ const OrdersTable = () => {
 
   const isChecked = (orderId) => checkedItems.includes(orderId);
 
-  const filteredOrders = orders.filter(
-    (order) =>
-      (filterStatus ? order.status === filterStatus : true) &&
-      (filterDate ? new Date(order.timestamp).toISOString().split('T')[0] === filterDate : true) &&
-      (order.products.some((product) =>
+  const filteredOrders = orders.filter((order) => {
+    const matchesStatus =
+      filterStatus === "Shipped Today"
+        ? order.timeline.some(
+            (event) =>  
+              event.status === "Shipped" &&
+              new Date(event.timestamp).toISOString().split('T')[0] === new Date().toISOString().split('T')[0]
+          )
+        : filterStatus === "Cancelled Today"
+        ? order.timeline.some(
+            (event) =>  
+              event.status === "Cancelled" &&
+              new Date(event.timestamp).toISOString().split('T')[0] === new Date().toISOString().split('T')[0]
+          )
+        : filterStatus
+        ? order.status === filterStatus
+        : true;
+
+    const matchesDate = filterDate
+      ? new Date(order.timestamp).toISOString().split('T')[0] === filterDate
+      : true;
+
+    const matchesPlatform = filterPlatform
+      ? order.sellingPlatform.name === filterPlatform
+      : true;
+
+    const matchesSearchTerm =
+      order.products.some((product) =>
         product.name.toLowerCase().includes(searchTerm.toLowerCase())
       ) ||
       order.courier.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
       order.trackingNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      order.sellingPlatform.name.toLowerCase().includes(searchTerm.toLowerCase())
-      || order.id.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  ).sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));;
+      order.sellingPlatform.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      order.id.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return matchesStatus && matchesDate && matchesPlatform && matchesSearchTerm;
+  }).sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
 
   const totalPages = Math.ceil(filteredOrders.length / ITEMS_PER_PAGE);
   const currentItems = filteredOrders.slice(
@@ -89,8 +126,91 @@ const OrdersTable = () => {
     setCurrentPage(page);
   };
 
+  const handlePageInputChange = (e) => {
+    const page = e.target.value;
+    if (!isNaN(page) && page > 0 && page <= totalPages) {
+      setInputPage(page);
+    } else {
+      setInputPage('');
+    }
+  };
+
+  const handlePageInputSubmit = () => {
+    if (inputPage) {
+      handlePageChange(Number(inputPage));
+    }
+  };
+
+  const renderPaginationButtons = () => {
+    const paginationButtons = [];
+    const startPage = Math.max(1, currentPage - Math.floor(MAX_PAGE_BUTTONS / 2));
+    const endPage = Math.min(totalPages, currentPage + Math.floor(MAX_PAGE_BUTTONS / 2));
+
+    if (currentPage > Math.floor(MAX_PAGE_BUTTONS / 2) + 1) {
+      paginationButtons.push(
+        <button
+          key={1}
+          className={`join-item btn ${currentPage === 1 ? 'btn-active' : ''}`}
+          onClick={() => handlePageChange(1)}
+        >
+          1
+        </button>
+      );
+      if (startPage > 2) {
+        paginationButtons.push(
+          <span key="start-ellipsis" className="join-item btn">...</span>
+        );
+      }
+    }
+
+    for (let i = startPage; i <= Math.min(endPage, totalPages); i++) {
+      paginationButtons.push(
+        <button
+          key={i}
+          className={`join-item btn ${currentPage === i ? 'btn-active' : ''}`}
+          onClick={() => handlePageChange(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    if (endPage < totalPages - 1) {
+      paginationButtons.push(
+        <span key="end-ellipsis" className="join-item btn">...</span>
+      );
+    }
+
+    if (endPage < totalPages) {
+      paginationButtons.push(
+        <button
+          key={totalPages}
+          className={`join-item btn ${currentPage === totalPages ? 'btn-active' : ''}`}
+          onClick={() => handlePageChange(totalPages)}
+        >
+          {totalPages}
+        </button>
+      );
+    }
+
+    paginationButtons.push(
+      <input
+        key="input"
+        type="text"
+        value={inputPage}
+        onChange={handlePageInputChange}
+        onKeyPress={(e) => e.key === 'Enter' && handlePageInputSubmit()}
+        className="join-item text-white input"
+        placeholder="Go To"
+        style={{ width: '80px' }}
+      />
+    );
+
+    return paginationButtons;
+  };
+
   return (
-    <div className="overflow-x-auto overflow-y-hidden min-h-96">
+    <div className="overflow-x-auto overflow-y-hidden min-h-[36rem]">
       <div className="flex items-center justify-center mb-4">
         <SearchIcon size={20} className="text-white mr-3" />
         <input
@@ -105,19 +225,17 @@ const OrdersTable = () => {
         <div className="dropdown dropdown-right mr-3">
           <label tabIndex={0} className="btn text-white">Bulk Actions</label>
           <ul tabIndex={0} className="dropdown-content text-white z-10 menu p-2 shadow bg-neutral rounded-box w-52">
-            <li><a onClick={() => setChangeStatus( (prev) => !prev)}>Change Status</a></li>
-            <li><a onClick={() => setChangePlatform( (prev) => !prev)}>Change Selling Platform</a></li>
+            <li><a onClick={() => setChangeStatus((prev) => !prev)}>Change Status</a></li>
+            <li><a onClick={() => setChangePlatform((prev) => !prev)}>Change Selling Platform</a></li>
           </ul>
         </div>
-        { changeStatus && (
+        {changeStatus && (
           <BulkEditStatusForm onClose={() => setChangeStatus(false)} checkedItems={checkedItems} />
-        )
-        }
+        )}
 
-        { changePlatform && (
+        {changePlatform && (
           <BulkEditPlatformForm onClose={() => setChangePlatform(false)} checkedItems={checkedItems} />
-        )
-        }
+        )}
 
         <button
           onClick={() => setAddOrder((prev) => !prev)}
@@ -126,7 +244,7 @@ const OrdersTable = () => {
           Create Order
         </button>
         <div className="dropdown dropdown-bottom ml-3">
-          <label tabIndex={0} className="btn text-white">Filter Status</label>
+          <label tabIndex={0} className="btn text-white flex flex-col">Filter Status{filterStatus !== "" && ( <p className=" text-xs">[{filterStatus}]</p>)} </label>
           <ul tabIndex={0} className="dropdown-content text-white z-10 menu p-2 shadow bg-neutral rounded-box w-52">
             <li><a onClick={() => handleStatusFilterChange("")}>All</a></li>
             <li><a onClick={() => handleStatusFilterChange("To Process")}>To Process</a></li>
@@ -134,9 +252,21 @@ const OrdersTable = () => {
             <li><a onClick={() => handleStatusFilterChange("Shipped")}>Shipped</a></li>
             <li><a onClick={() => handleStatusFilterChange("Completed")}>Completed</a></li>
             <li><a onClick={() => handleStatusFilterChange("Cancelled")}>Cancelled</a></li>
+            <li><a onClick={() => handleStatusFilterChange("Shipped Today")}>Shipped Today</a></li>
+            <li><a onClick={() => handleStatusFilterChange("Cancelled Today")}>Cancelled Today</a></li>
           </ul>
         </div>
-        
+
+        <div className="dropdown dropdown-bottom ml-3">
+          <label tabIndex={0} className="btn text-white flex flex-col">Filter Platform{filterPlatform !== "" && ( <p className=" text-xs">[{filterPlatform}]</p>)} </label>
+          <ul tabIndex={0} className="dropdown-content text-white z-10 menu p-2 shadow bg-neutral rounded-box w-52">
+            <li><a onClick={() => handlePlatformFilterChange("")}>All</a></li>
+            {salesplatforms.map((platform) => (
+              <li key={platform._id}><a onClick={() => handlePlatformFilterChange(platform.name)}>{platform.name}</a></li>
+            ))}
+          </ul>
+        </div>
+
         <div className="ml-3">
           <input
             type="date"
@@ -201,15 +331,7 @@ const OrdersTable = () => {
       </table>
       <div className="flex justify-center mt-4">
         <div className="join">
-          {[...Array(totalPages)].map((_, i) => (
-            <button
-              key={i}
-              onClick={() => handlePageChange(i + 1)}
-              className={`join-item btn ${currentPage === i + 1 ? "btn-active" : ""}`}
-            >
-              {i + 1}
-            </button>
-          ))}
+          {renderPaginationButtons()}
         </div>
       </div>
       {orderDetailsId && (
@@ -239,9 +361,6 @@ const TableContents = ({
   setOrderDetailsId,
 }) => {
   const [showProducts, setShowProducts] = useState(false);
-  if(status === "To Process"){
-    status = "To Process"
-}
   return (
     <>
       <tr className="border-none text-white bg-base-content">
--- frontend/src/components/table/RecordsHistory.jsx
+++ frontend/src/components/table/RecordsHistory.jsx
@@ -6,18 +6,105 @@ const RecordsHistory = ({ onClose }) => {
   const { histories } = useContext(PreferencesContext);
 
   const [currentPage, setCurrentPage] = useState(1);
+  const [inputPage, setInputPage] = useState('');
   const recordsPerPage = 10;
+  const maxPageButtons = 5;
 
   const histories_sorted = histories.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
 
-  
   const indexOfLastRecord = currentPage * recordsPerPage;
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   const currentRecords = histories_sorted.slice(indexOfFirstRecord, indexOfLastRecord);
 
   const totalPages = Math.ceil(histories_sorted.length / recordsPerPage);
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    setInputPage('');
+  };
+
+  const handlePageInputChange = (e) => {
+    const page = e.target.value;
+    if (!isNaN(page) && page > 0 && page <= totalPages) {
+      setInputPage(page);
+    } else {
+      setInputPage('');
+    }
+  };
+
+  const handlePageInputSubmit = () => {
+    if (inputPage) {
+      paginate(Number(inputPage));
+    }
+  };
+
+  const renderPaginationButtons = () => {
+    const paginationButtons = [];
+    const startPage = Math.max(1, currentPage - Math.floor(maxPageButtons / 2));
+    const endPage = Math.min(totalPages, currentPage + Math.floor(maxPageButtons / 2));
+
+    if (currentPage > Math.floor(maxPageButtons / 2) + 1) {
+      paginationButtons.push(
+        <button
+          key={1}
+          className={`join-item btn ${currentPage === 1 ? 'btn-active' : ''}`}
+          onClick={() => paginate(1)}
+        >
+          1
+        </button>
+      );
+      if (startPage > 2) {
+        paginationButtons.push(
+          <span key="start-ellipsis" className="join-item btn">...</span>
+        );
+      }
+    }
+
+    for (let i = startPage; i <= Math.min(endPage, totalPages); i++) {
+      paginationButtons.push(
+        <button
+          key={i}
+          className={`join-item btn ${currentPage === i ? 'btn-active' : ''}`}
+          onClick={() => paginate(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+
+    if (endPage < totalPages - 1) {
+      paginationButtons.push(
+        <span key="end-ellipsis" className="join-item btn">...</span>
+      );
+    }
+
+    if (endPage < totalPages) {
+      paginationButtons.push(
+        <button
+          key={totalPages}
+          className={`join-item btn ${currentPage === totalPages ? 'btn-active' : ''}`}
+          onClick={() => paginate(totalPages)}
+        >
+          {totalPages}
+        </button>
+      );
+    }
+
+    paginationButtons.push(
+      <input
+        key="input"
+        type="text"
+        value={inputPage}
+        onChange={handlePageInputChange}
+        onKeyPress={(e) => e.key === 'Enter' && handlePageInputSubmit()}
+        className="join-item text-white input"
+        placeholder="Go to"
+        style={{ width: '80px' }}
+      />
+    );
+
+    return paginationButtons;
+  };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
@@ -49,15 +136,7 @@ const RecordsHistory = ({ onClose }) => {
         </div>
         <div className="flex justify-center mt-4">
           <div className="join">
-            {[...Array(totalPages)].map((_, i) => (
-              <button
-                key={i}
-                className={`join-item btn ${currentPage === i + 1 ? 'btn-active' : ''}`}
-                onClick={() => paginate(i + 1)}
-              >
-                {i + 1}
-              </button>
-            ))}
+            {renderPaginationButtons()}
           </div>
         </div>
       </div>
