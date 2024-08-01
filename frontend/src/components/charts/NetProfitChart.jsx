94ef26710a3f9432249439c006627dd436fe3bf5
PolySpade <xudonald889@gmail.com>
2024-08-01 10:37:28 +0800

    Added accumulation

--- frontend/src/components/charts/NetProfitChart.jsx
+++ frontend/src/components/charts/NetProfitChart.jsx
@@ -6,7 +6,9 @@ const NetProfitChart = () => {
     const { ordersData: orders, expensesData: expenses, timeFrame } = useContext(ReportsContext);
 
     const totalexpenses = expenses.reduce((sum, expense) => sum + Math.round(expense.amount * 100) / 100, 0);
-    
+    let accumulatedProfit = 0;
+
+
     const parsedData = orders.map((data) => {
         const totalUnitCost = data.products.reduce((sum, product) => sum + (product.productId.unitCost * product.quantity), 0);
         const totalPaid = data.totalPaid;
@@ -14,7 +16,10 @@ const NetProfitChart = () => {
         
         const amount = parseFloat((totalPaid - totalUnitCost - otherFees - totalexpenses).toFixed(2));
         
-        return { timestamp: data.timestamp, amount };
+        accumulatedProfit += amount;
+        accumulatedProfit = parseFloat((accumulatedProfit).toFixed(2));
+
+        return { timestamp: data.timestamp, accumulatedProfit };
     });
 
     return (
